ac8bce907659d9719730c53f14b59276ff2c4519
Miguel Gonçalves <mglima1279@gmail.com>
2025-09-13 20:59:31 -0300

    Just creating my repository

---
+++ game/assets/app.js
@@ -0,0 +1,12 @@
+import { game } from "./module.js";
+const
+    getById = document.getElementById,
+    querrySelAll = document.querySelectorAll
+;
+const
+    canvas = getById("canvas"),
+    errorCells = querrySelAll(".errorCell"),
+    timeSlot = getById("timer")
+;
+
+cell.init()
---
+++ game/assets/module.js
@@ -0,0 +1,25 @@
+const colors = [
+    "#FF5733",
+    "#33FF57",
+    "#3357FF",
+    "#F1C40F",
+    "#8E44AD",
+    "#E67E22",
+    "#1ABC9C",
+    "#C0392B"
+];
+  
+const
+    getById = document.getElementById,
+    querrySelAll = document.querySelectorAll
+;
+
+const canvas = getById("canvas")
+
+export class game{
+    constructor() {
+        this.cell = class{
+
+        }
+    }
+}
